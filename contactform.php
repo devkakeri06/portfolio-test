935ee8ca7a60b483a567ddcc5c0e170d2283d493
Devaansh Kakeri <devaansh6@gmail.com>
2021-10-11 17:31:38 +0530

    Add files via upload

---
+++ contactform.php
@@ -0,0 +1,20 @@
+<?php
+    if (isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $mailFrom = $_POST['email'];
+        $message = $_POST['message'];
+
+        $mailTo = "kakeri43@gmail.com";
+        $headers = "From: ".$mailFrom;
+        $txt = "You have received an e-mail from ".$name."\n\n.$message;
+
+        mail($mailTo, $txt, $headers);
+        header("Location: index.php?mailsend");
+
+
+
+    }
+
+
+
+?>
